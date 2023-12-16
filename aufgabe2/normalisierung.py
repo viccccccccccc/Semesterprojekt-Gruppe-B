697b97911b2011af23447ffa81f0676db740cb66
Glenn <148215029+glenngru@users.noreply.github.com>
2023-12-16 14:17:41 +0100

    Update normalisierung.py
    Refactoring - TODO: Konstanten entfernen

--- aufgabe2/normalisierung.py
+++ aufgabe2/normalisierung.py
@@ -6,18 +6,15 @@ from torch.utils.data import Dataset, DataLoader
 
 
 class CustomDataset(Dataset):
-    def __init__(self, min_idx, excluded_max, reader):
-        self.reader = reader
-        self.min = min_idx
-        self.max = excluded_max
+    def __init__(self, my_reader, key_list):
+        self.reader = my_reader
+        self.key_list = key_list
 
     def __len__(self):
-        return self.max - self.min
+        return len(self.key_list)
 
     def __getitem__(self, idx):
-        if self.min + idx >= self.max:
-            raise Exception("Out of Bounds")
-        return self.reader[self.min + idx]
+        return self.reader[self.key_list[idx]]
 
 
 class H5Reader:
@@ -27,26 +24,27 @@ class H5Reader:
         self.active_normalize = False
         self.x_max = []
         self.y_max = []
-        random.shuffle(self.key_list)
 
     def __len__(self):
         return len(self.key_list)
 
     def __getitem__(self, idx):
-        if idx >= len(self.key_list):
-            raise Exception("Out of Bounds")
-        datapoint = self.file[self.key_list[idx]]
+        if isinstance(idx, str):
+            datapoint = self.file[idx]
+        else:
+            datapoint = self.file[self.key_list[idx]]
+
         x = datapoint["X"][:7]
         y = (datapoint["Y"][:][:]).flatten()
         x_div = x
         y_div = y
         if self.active_normalize:
-            print("Let's normalize")
             x_div = np.divide(x, self.x_max)
             y_div = np.divide(y, self.y_max)
         return x_div, y_div
 
-    def normalize(self, x_max, y_max):
+    def normalize(self):
+        x_max, y_max = self.find_max()
         self.active_normalize = True
         for i in range(len(x_max)):
             if x_max[i] == 0:
@@ -57,44 +55,34 @@ class H5Reader:
         self.x_max = x_max
         self.y_max = y_max
 
+    def split(self, anteil_test):
+        keys = self.key_list.copy()
+        random.shuffle(keys)
+        split_index = int(len(keys) * anteil_test)
+        test = keys[:split_index]
+        train = keys[split_index:]
 
-def find_max(h5_reader):
-    x_max, y_max = h5_reader[0]
-    for i in range(len(h5_reader)):
-        x, y = h5_reader[i]
-        x_max = [max(ai, bi) for ai, bi in zip(x_max, x)]
-        y_max = [max(ai, bi) for ai, bi in zip(y_max, y)]
-        print(i)
-    return x_max, y_max
+        return CustomDataset(self, train), CustomDataset(self, test)
 
-
-def train_test_split(anteil_test, hdf5_path):
-    reader = H5Reader(hdf5_path)
-    if os.path.exists('max_values/x.npz') and os.path.exists('max_values/y.npz'):
-        x_max = np.load(f'max_values/x.npz')
-        y_max = np.load(f'max_values/y.npz')
-        x_max = x_max['name1']
-        y_max = y_max['name1']
-    else:
-        x_max, y_max = find_max(reader)
-        np.savez(f'max_values/x.npz', name1=x_max)
-        np.savez(f'max_values/y.npz', name1=y_max)
-    print(x_max)
-    print(y_max)
-    reader.normalize(x_max, y_max)
-    split_index = int(len(reader) * (1 - anteil_test))
-    return CustomDataset(0, split_index, reader), CustomDataset(split_index, len(reader), reader)
+    def find_max(self):
+        if os.path.exists('max_values/x.npz') and os.path.exists('max_values/y.npz'):
+            x_max = np.load(f'max_values/x.npz')
+            y_max = np.load(f'max_values/y.npz')
+            x_max = x_max['name1']
+            y_max = y_max['name1']
+        else:
+            x_max, y_max = self[0]
+            for i in range(len(self)):
+                x, y = self[i]
+                x_max = [max(ai, bi) for ai, bi in zip(x_max, x)]
+                y_max = [max(ai, bi) for ai, bi in zip(y_max, y)]
+            np.savez(f'max_values/x.npz', name1=x_max)
+            np.savez(f'max_values/y.npz', name1=y_max)
+        return x_max, y_max
 
 
-#test_reader = H5Reader("data.h5")
-#x_maximum, y_maximum = find_max(test_reader)
-
-#print(len(x_maximum))
-#print(x_maximum)
-#print(len(y_maximum))
-#print(y_maximum)
-
-#train_dataset, test_dataset = train_test_split(1. / 3, "data.h5")
-#train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-#test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False)
-
+reader = H5Reader("data.h5")
+reader.normalize()
+train_dataset, test_dataset = reader.split(1. / 3)
+train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False)

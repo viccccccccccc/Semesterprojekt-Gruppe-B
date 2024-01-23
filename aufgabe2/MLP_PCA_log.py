3656939f2371bb5ec8fe2018953f66866d5c8b85
viccccccccccc <76782121+viccccccccccc@users.noreply.github.com>
2024-01-23 11:09:07 +0100

    Update MLP_PCA_log.py
    pfad war falsch

--- aufgabe2/MLP_PCA_log.py
+++ aufgabe2/MLP_PCA_log.py
@@ -292,7 +292,7 @@ def train():
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print(device)
-reader = H5Reader("/vol/tmp/feuerforsp/gruppe_b/pca256.h5")
+reader = H5Reader("/vol/tmp/feuforsp/gruppe_b/pca256.h5")
 reader.normalize()
 train_dataset, test_dataset = reader.split(test_train_split)
 train_dataloader = DataLoader(train_dataset, batch_size=batch_size, num_workers=72, shuffle=True)
